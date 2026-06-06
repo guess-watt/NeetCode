b7f01c3d6e54a3939ccc0d8fbc7483d27e2ee877
AWN <anwinasprogramer@gmail.com>
2026-06-06 23:51:57 +0530

    Add: sort-array-by-increasing-frequency - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        result = {}
+
+        for i in nums:
+            if i not in result:
+                result[i] = 1
+            else:
+                result[i] += 1
+
+
+        nums.sort(key=lambda x: (result[x], -x)) #need to what exactly happens behind this
+        return nums
+        
+
+
+
+        
