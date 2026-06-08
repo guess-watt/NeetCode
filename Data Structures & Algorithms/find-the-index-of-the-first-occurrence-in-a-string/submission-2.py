0fa92e145f502fbe38aaefe3f5ac674e55f6c5af
AWN <anwinasprogramer@gmail.com>
2026-06-08 23:51:26 +0530

    Add comment to clarify find() usage in strStr method
    Added a comment explaining the use of the find() method.

--- Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-2.py
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-2.py
@@ -1,6 +1,8 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        
+        return haystack.find(needle)            #find() used to find the position of the sub-string
+        
         
 
 
