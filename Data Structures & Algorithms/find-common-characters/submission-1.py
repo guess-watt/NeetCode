e716ca7608ae6a8d5d486439480695d456d63957
AWN <anwinasprogramer@gmail.com>
2026-06-14 22:33:45 +0530

    Add: find-common-characters - submission-1

---
+++ Data Structures & Algorithms/find-common-characters/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        words = sorted(words, key=len)
+        result = []
+        for i in words[0]:
+            for j in range(1,len(words)):
+                if i not in words[j]:
+                    break
+                else:
+                    words[j]=words[j].replace(i,"",1)
+            else:
+                result.append(i)        #for - else loop working is based on the inner break condition. if we 
+                                        #  reach inner break, outer else will not work. 
+            
+        return result
+            
+
+
+        
