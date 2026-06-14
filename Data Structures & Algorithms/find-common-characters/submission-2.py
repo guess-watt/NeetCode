13cddd8b1afd8762b45044bfa214accb3713009a
AWN <anwinasprogramer@gmail.com>
2026-06-14 22:45:56 +0530

    Add: find-common-characters - submission-2

---
+++ Data Structures & Algorithms/find-common-characters/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        # words = sorted(words, key=len)
+        # result = []
+        # for i in words[0]:
+        #     for j in range(1,len(words)):
+        #         if i not in words[j]:
+        #             break
+        #         else:
+        #             words[j]=words[j].replace(i,"",1)
+        #     else:
+        #         result.append(i)        #for - else loop working is based on the inner break condition. if we 
+        #                                 #  reach inner break, outer else will not work. 
+            
+        # return result
+            
+        from collections import Counter
+
+        common = Counter(words[0])
+
+        for word in words[1:]:
+            common &= Counter(word)
+
+        result = []
+
+        for ch, freq in common.items():
+            result.extend([ch]*freq)
+        return result
+
+
+        
