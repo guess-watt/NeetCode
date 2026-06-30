bb9d95d67206607a67de649b0a378b1ae55b12eb
AWN <anwinasprogramer@gmail.com>
2026-06-30 23:38:17 +0530

    Add: word-pattern - submission-1

---
+++ Data Structures & Algorithms/word-pattern/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        
+        forward,backward = {},{}
+
+        for c,w in zip(pattern,words):
+            if c in forward and forward[c] != w:
+                return False
+            if w in backward and backward[w] != c:
+                return False
+            forward[c] = w
+            backward[w] = c
+
+        return True
+
+        
