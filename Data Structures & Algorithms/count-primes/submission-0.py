f1b43b05f5508e5a9e3c24d83c545457696d3231
AWN <anwinasprogramer@gmail.com>
2026-07-05 22:25:22 +0530

    Add: count-primes - submission-0

---
+++ Data Structures & Algorithms/count-primes/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        
+        isprime = [True]*n
+
+        isprime[0] = isprime[1] = False
+
+        for i in range(2,int(n**0.5)+1):
+            if isprime[i] == True:
+                for j in range(i*i,n,i):
+                    isprime[j] = False
+        return sum(isprime)
