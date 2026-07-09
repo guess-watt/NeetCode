431472f5d8f12cf8f99e415e45837a53e8352e0e
AWN <anwinasprogramer@gmail.com>
2026-07-09 23:54:49 +0530

    Add: redistribute-characters-to-make-all-strings-equal - submission-3

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        
+
+        result = ""
+        hashdict = {}
+        for i in words:
+            result += i
+        for i in result:
+            if i not in hashdict:
+                hashdict[i] = 1
+            else:
+                hashdict[i] += 1
+        
+        n = len(words)
+
+        for count in hashdict.values():
+            if count % n != 0:
+                return False
+        return True
+
+        """
+            The solution can be improved by avoiding string concatenation
+            Since strings are immutable in Python, each concatenation creates a new string,
+            resulting in unnecessary time and space overhead.
+        """
+
+
+
+
+
+
+        
