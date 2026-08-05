4911fa9359a6d26a2031318069e76e28cc1fb0ad
AWN <anwinasprogramer@gmail.com>
2026-08-06 00:38:07 +0530

    Add: find-in-mountain-array - submission-1

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-1.py
@@ -0,0 +1,50 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+
+        n = mountainArr.length()
+
+        # Step 1: Find the peak
+        left = 0
+        right = n - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if mountainArr.get(mid) < mountainArr.get(mid + 1):
+                left = mid + 1
+            else:
+                right = mid
+
+        peak = left
+
+        # Step 2: Binary search on increasing part
+        left = 0              # <-- CHANGED
+        right = peak          # <-- CHANGED
+
+        while left <= right:
+            mid = (left + right) // 2
+            value = mountainArr.get(mid)
+
+            if value == target:
+                return mid
+            elif value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        # Step 3: Binary search on decreasing part
+        left = peak + 1
+        right = n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            value = mountainArr.get(mid)
+
+            if value == target:
+                return mid
+            elif value < target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
