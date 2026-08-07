be9c345c1ac443a443456a59105b3d327f6f5084
AWN <anwinasprogramer@gmail.com>
2026-08-07 23:45:38 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,51 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        ref = set()
+
+    
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+
+                if board[i][j] not in ref:
+                    ref.add(board[i][j])
+                else:
+                    return False
+
+            ref.clear()
+
+
+
+        for j in range(9):
+            ref = set()
+
+            for i in range(9):
+                if board[i][j] == ".":
+                    continue
+
+                if board[i][j] in ref:
+                    return False
+
+                ref.add(board[i][j])
+
+
+        # Check 3x3 boxes
+        for row in range(0, 9, 3):
+            for col in range(0, 9, 3):
+
+                ref = set()
+
+                for i in range(row, row + 3):
+                    for j in range(col, col + 3):
+
+                        if board[i][j] == ".":
+                            continue
+
+                        if board[i][j] in ref:
+                            return False
+
+                        ref.add(board[i][j])
+
+        return True
