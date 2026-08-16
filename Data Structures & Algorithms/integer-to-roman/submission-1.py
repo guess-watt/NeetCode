32ff1a58998ee83af73eb3ed85e873d667594106
AWN <anwinasprogramer@gmail.com>
2026-08-16 18:48:46 +0530

    Add: integer-to-roman - submission-1

---
+++ Data Structures & Algorithms/integer-to-roman/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        ref = [["I",1],["IV",4],["V",5],["IX",9],["X",10],["XL",40],["L",50],
+        ["XC",90],["C",100],["CD",400],["D",500],["CM",900],["M",1000]]
+        ## we define all numbers first,special number like 3,9,40 etc are also defiended
+        ## as it first come with small and then larger(IV,IX) unlike the traditional setup of small after 
+        ## large(XI,LX)
+
+
+        res = ""
+
+        for alp,value in reversed(ref):
+            if num//value:
+                count = num//value
+                res += (alp * count)
+                num %= value
+        
+        return res
