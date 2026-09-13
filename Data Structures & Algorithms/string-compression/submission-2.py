40455aa172bcd2d0103826342d9dea4e8517cc69
AWN <anwinasprogramer@gmail.com>
2026-09-13 21:44:59 +0530

    Add: string-compression - submission-2

---
+++ Data Structures & Algorithms/string-compression/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1:
+            return 1
+
+        ref = chars[0]
+        count = 1
+        op = ""
+
+        for i in range(1, len(chars)):
+            if chars[i] == ref:
+                count += 1
+            else:
+                op += ref
+
+                if count > 1:
+                    op += str(count)
+
+                ref = chars[i]
+                count = 1
+
+        # Add the last group
+        op += ref
+
+        if count > 1:
+            op += str(count)
+
+        # Copy compressed result into chars
+        chars[:] = list(op)
+
+        return len(op)
+
+        
